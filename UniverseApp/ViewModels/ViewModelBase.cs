90fc9faf0c7b84bee44d12a5a16c618ae583cded
mewosh <milosz.samotyjak@gmail.com>
2023-09-28 15:12:26 +0200

    Init

---
+++ UniverseApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace UniverseApp.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ UniverseApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace UniverseApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
